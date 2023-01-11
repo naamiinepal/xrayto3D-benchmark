4f6d0e34d540a34a374fbe55fff8942496ef3456
msrepo <maheshshakya.01@gmail.com>
2023-01-11 13:57:04 +0545

    parameterize transforms by size

--- XrayTo3DShape/datasets/transforms.py
+++ XrayTo3DShape/datasets/transforms.py
@@ -3,18 +3,22 @@ import numpy as np
 from monai.data.image_reader import PILReader
 import einops
 
-def get_lidc_transforms():
+def get_nonkasten_transforms(size=64,resolution=1.5):
     lidc_ap_transform = Compose(
         [
             LoadImageD(
                 keys={"ap"},
                 ensure_channel_first=False,
-                dtype=np.uint8,
+                dtype=np.float32,
                 simple_keys=True,
                 image_only=False,
                 reader=PILReader(converter=lambda image: image.rotate(90)),
             ),
-            LambdaD(keys={"ap"}, func=lambda t: einops.repeat(t, "m n -> k m n", k=96)),
+            EnsureChannelFirstD(keys='ap'),
+            ResizeD(keys={'ap'},spatial_size=[size,size],size_mode='all',mode='bilinear'),
+            # LambdaD(keys={"ap"}, func=lambda t: einops.repeat(t, "1 m n -> 1 k m n", k=size)),
+            ScaleIntensityD(keys={'ap'}),
+            # DataStatsD(keys='ap')
         ]
     )
     lidc_lat_transform = Compose(
@@ -22,12 +26,16 @@ def get_lidc_transforms():
             LoadImageD(
                 keys={"lat"},
                 ensure_channel_first=False,
-                dtype=np.uint8,
+                dtype=np.float32,
                 simple_keys=True,
                 image_only=False,
                 reader=PILReader(converter=None),
             ),
-            LambdaD(keys={"lat"}, func=lambda t: einops.repeat(t, "m n -> m n k", k=96)),
+            EnsureChannelFirstD(keys='lat'),
+            ResizeD(keys={'lat'},spatial_size=[size,size],size_mode='all',mode='bilinear'),            
+            # LambdaD(keys={"lat"}, func=lambda t: einops.repeat(t, "1 m n -> 1 m n k", k=size)),
+            ScaleIntensityD(keys={'lat'}),
+            # DataStatsD(keys='lat')
         ]
     )
     lidc_seg_transform = Compose(
@@ -35,18 +43,78 @@ def get_lidc_transforms():
             LoadImageD(
                 keys={"seg"},
                 ensure_channel_first=True,
-                dtype=np.uint8,
+                dtype=np.float32,
                 simple_keys=True,
                 image_only=False,
             ),
             SpacingD(
                 keys={"seg"},
-                pixdim=(1, 1, 1),
+                pixdim=(resolution, resolution, resolution),
                 mode="nearest",
                 padding_mode="zeros",
             ),
-            SpatialPadD(keys={"seg"}, spatial_size=(96, 96, 96)),
+            SpatialPadD(keys={"seg"}, spatial_size=(size, size, size)),
             OrientationD(keys={"seg"}, axcodes="PIR"),
+            ThresholdIntensityD(keys='seg',threshold=0.5, above=False,cval=1.0),
+            # DataStatsD(keys='seg')
+        ]
+    )
+    return {'ap':lidc_ap_transform,'lat':lidc_lat_transform,'seg':lidc_seg_transform}
+
+def get_kasten_transforms(size=64,resolution=1.5):
+    lidc_ap_transform = Compose(
+        [
+            LoadImageD(
+                keys={"ap"},
+                ensure_channel_first=False,
+                dtype=np.float32,
+                simple_keys=True,
+                image_only=False,
+                reader=PILReader(converter=lambda image: image.rotate(90)),
+            ),
+            EnsureChannelFirstD(keys='ap'),
+            ResizeD(keys={'ap'},spatial_size=[size,size],size_mode='all',mode='bilinear'),
+            LambdaD(keys={"ap"}, func=lambda t: einops.repeat(t, "1 m n -> 1 k m n", k=size)),
+            ScaleIntensityD(keys={'ap'}),
+            # DataStatsD(keys='ap')
+        ]
+    )
+    lidc_lat_transform = Compose(
+        [
+            LoadImageD(
+                keys={"lat"},
+                ensure_channel_first=False,
+                dtype=np.float32,
+                simple_keys=True,
+                image_only=False,
+                reader=PILReader(converter=None),
+            ),
+            EnsureChannelFirstD(keys='lat'),
+            ResizeD(keys={'lat'},spatial_size=[size,size],size_mode='all',mode='bilinear'),            
+            LambdaD(keys={"lat"}, func=lambda t: einops.repeat(t, "1 m n -> 1 m n k", k=size)),
+            ScaleIntensityD(keys={'lat'}),
+            # DataStatsD(keys='lat')
+        ]
+    )
+    lidc_seg_transform = Compose(
+        [
+            LoadImageD(
+                keys={"seg"},
+                ensure_channel_first=True,
+                dtype=np.float32,
+                simple_keys=True,
+                image_only=False,
+            ),
+            SpacingD(
+                keys={"seg"},
+                pixdim=(resolution, resolution, resolution),
+                mode="nearest",
+                padding_mode="zeros",
+            ),
+            SpatialPadD(keys={"seg"}, spatial_size=(size, size, size)),
+            OrientationD(keys={"seg"}, axcodes="PIR"),
+            ThresholdIntensityD(keys='seg',threshold=0.5, above=False,cval=1.0),
+            # DataStatsD(keys='seg')
         ]
     )
     return {'ap':lidc_ap_transform,'lat':lidc_lat_transform,'seg':lidc_seg_transform}
