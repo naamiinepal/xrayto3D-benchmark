2e55b6e906b2863daa683029120beeca861da036
msrepo <maheshshakya.01@gmail.com>
2023-01-11 15:25:10 +0545

    denoising Autoencoder transform

--- XrayTo3DShape/datasets/transforms.py
+++ XrayTo3DShape/datasets/transforms.py
@@ -2,9 +2,47 @@ from monai.transforms import *
 import numpy as np
 from monai.data.image_reader import PILReader
 import einops
+import torch
+from skimage.util import random_noise
 
-def get_nonkasten_transforms(size=64,resolution=1.5):
-    lidc_ap_transform = Compose(
+
+def get_denoising_autoencoder_transforms(size=64, resolution=1.5):
+
+    NoiseLambda = Lambda(lambda d: {
+        "orig": d["seg"],
+        "gaus": torch.tensor(
+            random_noise(d["seg"], mode='gaussian',mean=0,var=0.01), dtype=torch.float32),
+        "s&p": torch.tensor(random_noise(d["seg"], mode='s&p', salt_vs_pepper=0.1)),
+    })
+
+    seg_transform = Compose(
+        [
+            LoadImageD(
+                keys={"seg"},
+                ensure_channel_first=True,
+                dtype=np.float32,
+                simple_keys=True,
+                image_only=False,
+            ),
+            SpacingD(
+                keys={"seg"},
+                pixdim=(resolution, resolution, resolution),
+                mode="nearest",
+                padding_mode="zeros",
+            ),
+            SpatialPadD(keys={"seg"}, spatial_size=(size, size, size)),
+            OrientationD(keys={"seg"}, axcodes="PIR"),
+            ThresholdIntensityD(keys="seg", threshold=0.5, above=False, cval=1.0),
+            NoiseLambda
+            # DataStatsD(keys='seg')
+        ]
+    )
+    callable_identity_transform = lambda x: x
+    return {'ap':callable_identity_transform, 'lat':callable_identity_transform, 'seg':seg_transform}
+
+
+def get_nonkasten_transforms(size=64, resolution=1.5):
+    ap_transform = Compose(
         [
             LoadImageD(
                 keys={"ap"},
@@ -14,14 +52,14 @@ def get_nonkasten_transforms(size=64,resolution=1.5):
                 image_only=False,
                 reader=PILReader(converter=lambda image: image.rotate(90)),
             ),
-            EnsureChannelFirstD(keys='ap'),
-            ResizeD(keys={'ap'},spatial_size=[size,size],size_mode='all',mode='bilinear'),
+            EnsureChannelFirstD(keys="ap"),
+            ResizeD(keys={"ap"}, spatial_size=[size, size], size_mode="all", mode="bilinear"),
             # LambdaD(keys={"ap"}, func=lambda t: einops.repeat(t, "1 m n -> 1 k m n", k=size)),
-            ScaleIntensityD(keys={'ap'}),
+            ScaleIntensityD(keys={"ap"}),
             # DataStatsD(keys='ap')
         ]
     )
-    lidc_lat_transform = Compose(
+    lat_transform = Compose(
         [
             LoadImageD(
                 keys={"lat"},
@@ -31,14 +69,14 @@ def get_nonkasten_transforms(size=64,resolution=1.5):
                 image_only=False,
                 reader=PILReader(converter=None),
             ),
-            EnsureChannelFirstD(keys='lat'),
-            ResizeD(keys={'lat'},spatial_size=[size,size],size_mode='all',mode='bilinear'),            
+            EnsureChannelFirstD(keys="lat"),
+            ResizeD(keys={"lat"}, spatial_size=[size, size], size_mode="all", mode="bilinear"),
             # LambdaD(keys={"lat"}, func=lambda t: einops.repeat(t, "1 m n -> 1 m n k", k=size)),
-            ScaleIntensityD(keys={'lat'}),
+            ScaleIntensityD(keys={"lat"}),
             # DataStatsD(keys='lat')
         ]
     )
-    lidc_seg_transform = Compose(
+    seg_transform = Compose(
         [
             LoadImageD(
                 keys={"seg"},
@@ -55,14 +93,15 @@ def get_nonkasten_transforms(size=64,resolution=1.5):
             ),
             SpatialPadD(keys={"seg"}, spatial_size=(size, size, size)),
             OrientationD(keys={"seg"}, axcodes="PIR"),
-            ThresholdIntensityD(keys='seg',threshold=0.5, above=False,cval=1.0),
+            ThresholdIntensityD(keys="seg", threshold=0.5, above=False, cval=1.0),
             # DataStatsD(keys='seg')
         ]
     )
-    return {'ap':lidc_ap_transform,'lat':lidc_lat_transform,'seg':lidc_seg_transform}
+    return {"ap": ap_transform, "lat": lat_transform, "seg": seg_transform}
 
-def get_kasten_transforms(size=64,resolution=1.5):
-    lidc_ap_transform = Compose(
+
+def get_kasten_transforms(size=64, resolution=1.5):
+    ap_transform = Compose(
         [
             LoadImageD(
                 keys={"ap"},
@@ -72,14 +111,14 @@ def get_kasten_transforms(size=64,resolution=1.5):
                 image_only=False,
                 reader=PILReader(converter=lambda image: image.rotate(90)),
             ),
-            EnsureChannelFirstD(keys='ap'),
-            ResizeD(keys={'ap'},spatial_size=[size,size],size_mode='all',mode='bilinear'),
+            EnsureChannelFirstD(keys="ap"),
+            ResizeD(keys={"ap"}, spatial_size=[size, size], size_mode="all", mode="bilinear"),
             LambdaD(keys={"ap"}, func=lambda t: einops.repeat(t, "1 m n -> 1 k m n", k=size)),
-            ScaleIntensityD(keys={'ap'}),
+            ScaleIntensityD(keys={"ap"}),
             # DataStatsD(keys='ap')
         ]
     )
-    lidc_lat_transform = Compose(
+    lat_transform = Compose(
         [
             LoadImageD(
                 keys={"lat"},
@@ -89,14 +128,14 @@ def get_kasten_transforms(size=64,resolution=1.5):
                 image_only=False,
                 reader=PILReader(converter=None),
             ),
-            EnsureChannelFirstD(keys='lat'),
-            ResizeD(keys={'lat'},spatial_size=[size,size],size_mode='all',mode='bilinear'),            
+            EnsureChannelFirstD(keys="lat"),
+            ResizeD(keys={"lat"}, spatial_size=[size, size], size_mode="all", mode="bilinear"),
             LambdaD(keys={"lat"}, func=lambda t: einops.repeat(t, "1 m n -> 1 m n k", k=size)),
-            ScaleIntensityD(keys={'lat'}),
+            ScaleIntensityD(keys={"lat"}),
             # DataStatsD(keys='lat')
         ]
     )
-    lidc_seg_transform = Compose(
+    seg_transform = Compose(
         [
             LoadImageD(
                 keys={"seg"},
@@ -113,9 +152,8 @@ def get_kasten_transforms(size=64,resolution=1.5):
             ),
             SpatialPadD(keys={"seg"}, spatial_size=(size, size, size)),
             OrientationD(keys={"seg"}, axcodes="PIR"),
-            ThresholdIntensityD(keys='seg',threshold=0.5, above=False,cval=1.0),
+            ThresholdIntensityD(keys="seg", threshold=0.5, above=False, cval=1.0),
             # DataStatsD(keys='seg')
         ]
     )
-    return {'ap':lidc_ap_transform,'lat':lidc_lat_transform,'seg':lidc_seg_transform}
-
+    return {"ap": ap_transform, "lat": lat_transform, "seg": seg_transform}
