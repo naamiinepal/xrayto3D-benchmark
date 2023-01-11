5bf4f6d1d4baff0dbba6678ce37db9828d4ad2b8
msrepo <maheshshakya.01@gmail.com>
2023-01-11 17:17:12 +0545

    architecture pipeline tests

--- tests/test_chen_pipeline.py
+++ tests/test_chen_pipeline.py
@@ -9,7 +9,10 @@ import wandb
 
 lr = 1e-2
 NUM_EPOCHS = 1000
-wandb.init(project='pipeline-test-01',name='chen-01')
+WANDB_ON = True
+
+if WANDB_ON:
+    wandb.init(project='pipeline-test-01',name='chen-NGCC-01')
 
 paths_location = 'configs/test/LIDC-DRR-test.csv'
 paths = pd.read_csv(paths_location,index_col=0).to_numpy()
@@ -34,6 +37,7 @@ pred_tensor = model(ap_tensor,lat_tensor)
 print(pred_tensor.shape)
 
 loss_function = DiceCELoss(sigmoid=True)
+ngcc_loss_function = NGCCLoss()
 optimizer = torch.optim.AdamW(model.parameters(),lr)
 dice_metric_evaluator = DiceMetric(include_background=False)
 
@@ -49,10 +53,13 @@ for i in range(NUM_EPOCHS):
             Activations(sigmoid=True),
             AsDiscrete(threshold=0.5)
         ])
+        ap_pred, _, lat_pred = get_projectionslices_from_3d(pred_seg_logits)
+        ngcc_loss = ngcc_loss_function(ap_pred,ap_tensor)
         pred_seg = eval_transform(pred_seg_logits)
         dice_metric_evaluator(y_pred=pred_seg, y=seg_tensor)
     
     acc = dice_metric_evaluator.aggregate()
     dice_metric_evaluator.reset()
-
-    wandb.log({'loss': loss.item(), 'accuracy': acc.item()})
+    if WANDB_ON:
+        wandb.log({'loss': loss.item(), 'accuracy': acc.item(),'1-NGCC': ngcc_loss.item()})
+    print(f'loss {loss.item():.4f} accuracy {acc.item():.4f} NGCC {ngcc_loss.item():.4f}')
