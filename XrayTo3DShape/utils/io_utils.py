d3e354325004b9ebe0067839bf5068896e58df88
mahesh shakya <mahesh.shakya@naamii.org.np>
2023-01-26 09:15:15 +0545

    add nifti saver

--- XrayTo3DShape/utils/io_utils.py
+++ XrayTo3DShape/utils/io_utils.py
@@ -1,5 +1,10 @@
 from pathlib import Path
 import SimpleITK as sitk
+from monai.data.nifti_saver import NiftiSaver
+from pytorch_lightning.callbacks import BasePredictionWriter
+import pytorch_lightning as pl
+from typing import Any,Sequence,Optional
+import numpy as np
 
 def write_image(img, out_path,pixeltype=None):
     if isinstance(out_path, Path):
@@ -31,3 +36,14 @@ def read_image(img_path):
     #     raise ValueError(f'ImagePixelType cannot be {pixeltype}')
 
     return sitk.ReadImage(img_path)
+
+
+class NiftiPredictionWriter(BasePredictionWriter):
+    def __init__(self, output_dir, write_interval: str = "batch") -> None:
+        super().__init__(write_interval)
+        self.output_dir = output_dir
+        self.nifti_saver = NiftiSaver(output_dir=self.output_dir,output_postfix='pred',resample=True,dtype=np.int16,separate_folder=False)
+    
+    def write_on_batch_end(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule", prediction: Any, batch_indices: Optional[Sequence[int]], batch: Any, batch_idx: int, dataloader_idx: int) -> None:
+        self.nifti_saver.save_batch(prediction['pred_seg'],prediction['pred_seg_meta_dict'])
+
