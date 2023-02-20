fa8a16d08d2ce8f75b6af05fce07aa7cfd8cefb3
mahesh shakya <mahesh.shakya@naamii.org.np>
2023-02-20 14:38:45 +0545

    pass loss name, anatomy

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import wandb
+import torch
 from torch.utils.data.dataloader import DataLoader
 from torch.optim import Adam
 import pytorch_lightning as pl
@@ -13,7 +14,8 @@ from XrayTo3DShape import (
     MetricsLogger,
     parse_training_arguments,
     get_model,
-    get_model_config
+    get_model_config,
+    get_loss
 )
 import XrayTo3DShape
 from monai.utils.misc import set_determinism
@@ -30,13 +32,15 @@ if __name__ == "__main__":
     lr = 1e-2
     NUM_EPOCHS = args.epochs
     IMG_SIZE = args.size
+    ANATOMY = args.anatomy
+    LOSS_NAME = args.loss
     IMG_RESOLUTION = args.res
     BATCH_SIZE = args.batch_size
     WANDB_PROJECT = "pipeline-test-01"
     model_name = args.model_name
     experiment_name = args.experiment_name
     WANDB_EXPERIMENT_GROUP = args.model_name
-    WANDB_TAGS = [WANDB_EXPERIMENT_GROUP,'model_selection',*args.tags]
+    WANDB_TAGS = [WANDB_EXPERIMENT_GROUP,ANATOMY,LOSS_NAME,'model_selection',*args.tags]
 
     set_determinism(seed=SEED)
     seed_everything(seed=SEED)
@@ -66,8 +70,9 @@ if __name__ == "__main__":
     model = get_model(model_name=args.model_name,image_size=IMG_SIZE)
     MODEL_CONFIG = get_model_config(model_name,IMG_SIZE)
 
-    loss_function = DiceLoss(sigmoid=True)
-    # loss_function = BCEWithLogitsLoss()
+    # loss_function = DiceLoss(sigmoid=True)
+
+    loss_function = get_loss(loss_name=LOSS_NAME,anatomy=ANATOMY,image_size=IMG_SIZE,lambda_bce=args.lambda_bce,lambda_dice=args.lambda_dice) 
     optimizer = Adam(model.parameters(), lr)
     # load pytorch lightning module
     experiment:BaseExperiment = getattr(XrayTo3DShape.experiments,experiment_name)(model,optimizer,loss_function,BATCH_SIZE)
@@ -75,7 +80,10 @@ if __name__ == "__main__":
     # batch = next(iter(train_loader))
     # input,output = experiment.get_input_output_from_batch(batch)
     # pred_logits = experiment.model(*input)
+    # bs = pred_logits.shape[0]
+    # print('pred shape',pred_logits.shape)
     # loss = experiment.loss_function(pred_logits,output)
+    # # loss = experiment.loss_function(pred_logits.reshape(bs,-1),output.reshape(bs,-1))
     # print(loss)
     evaluation_callbacks = []
     if args.evaluate:
@@ -85,7 +93,7 @@ if __name__ == "__main__":
         
         metric_saver = MetricsLogger(output_dir=args.output_dir,voxel_spacing=  IMG_RESOLUTION,nsd_tolerance=1)
         evaluation_callbacks.append(metric_saver)
-        
+
         trainer = pl.Trainer(callbacks=evaluation_callbacks)
         trainer.predict(model=experiment,ckpt_path=args.checkpoint_path,dataloaders=val_loader,return_predictions=False)
         
