4abd3ddfeff4dacc3510166962fce278ca20ac94
mahesh shakya <mahesh.shakya@naamii.org.np>
2023-02-25 20:20:26 +0545

    refactor: calculate flattened vec size

--- XrayTo3DShape/architectures/utils.py
+++ XrayTo3DShape/architectures/utils.py
@@ -1,13 +1,22 @@
 from typing import Sequence
-def calculate_1d_vec_channels(image_size,encoder_strides:Sequence,encoder_last_channel)->int:
+def calculate_1d_vec_channels(spatial_dims,image_size,encoder_strides:Sequence,encoder_last_channel)->int:
     """calculate size of the 1D Low-dim embedding for given model layers"""
-    iw,ih = image_size,image_size # width , height
     
     for stride in encoder_strides:
-        iw /= stride
-        ih /= stride
-    lat_encoder_1d_vecsize = ap_encoder_1d_vecsize = ih * iw * encoder_last_channel
+        image_size /= stride
+
+    lat_encoder_1d_vecsize = ap_encoder_1d_vecsize = (image_size**spatial_dims) * encoder_last_channel
 
     decoded_cube_channels = ap_encoder_1d_vecsize  + lat_encoder_1d_vecsize
 
+
     return int(decoded_cube_channels)
+
+
+if __name__ == '__main__':
+    from monai.networks.layers.convutils import calculate_out_shape, same_padding
+
+    print(calculate_1d_vec_channels(2,128,(2,2,2,2),32))
+    strides = (2,2,2)
+    ks = 3
+    print(calculate_out_shape(128,ks,strides,same_padding(ks,1)))
