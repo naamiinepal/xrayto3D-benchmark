9dfd190b4c5972b83d11911080a8ff75c7cb3787
mahesh shakya <mahesh.shakya@naamii.org.np>
2023-02-25 17:12:30 +0545

    one-hot transform

--- XrayTo3DShape/transforms/post_transform.py
+++ XrayTo3DShape/transforms/post_transform.py
@@ -1,3 +1,4 @@
 from monai.transforms import Compose,Activations,AsDiscrete
 
 post_transform = Compose([Activations(sigmoid=True), AsDiscrete(threshold=0.5)])
+post_transform_oh = Compose([AsDiscrete(argmax=True)])
