49b3222a03e7ef43c62b724cf16c9202c7cca5b3
mahesh shakya <mahesh.shakya@naamii.org.np>
2023-02-25 12:33:49 +0545

    do not clip noise

--- XrayTo3DShape/transforms/base_transforms.py
+++ XrayTo3DShape/transforms/base_transforms.py
@@ -15,7 +15,7 @@ def get_denoising_autoencoder_transforms(size=64, resolution=1.5):
     NoiseLambda = Lambda(lambda d: {
         "orig": d["seg"],
         "gaus": torch.tensor(
-            random_noise(d["seg"], mode='gaussian',mean=0,var=0.01), dtype=torch.float32),
+            random_noise(d["seg"], mode='gaussian',mean=0,var=0.01,clip=False), dtype=torch.float32),
     })
 
     seg_transform = Compose(
