15487193957714ca522a31f9066627b71fe4096c
mahesh shakya <mahesh.shakya@naamii.org.np>
2023-04-01 13:05:50 +0545

    add unetr

--- bash_scripts/evaluate_verse19_dropout.sh
+++ bash_scripts/evaluate_verse19_dropout.sh
@@ -8,3 +8,5 @@ python evaluate.py  --testpaths configs/paths/verse19/Verse2019-DRR-full_test.cs
 
 python evaluate.py  --testpaths configs/paths/verse19/Verse2019-DRR-full_test.csv --gpu 0 --image_size 64 --batch_size 8 --accelerator gpu --res 1.5 --model_name UNet --ckpt_path runs/2d-3d-benchmark/30wlxp31/checkpoints --gpu 0
 
+python evaluate.py  --testpaths configs/paths/verse19/Verse2019-DRR-full_test.csv --gpu 0 --image_size 64 --batch_size 8 --accelerator gpu --res 1.5 --model_name UNETR --ckpt_path runs/2d-3d-benchmark/0ugb85wj/checkpoints --gpu 0
+
