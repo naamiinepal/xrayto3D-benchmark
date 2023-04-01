3f930dd01ef2c426e56ff51635615de71caf04cb
mahesh shakya <mahesh.shakya@naamii.org.np>
2023-04-01 07:49:52 +0545

    authorship

--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
 from setuptools import find_packages, setup
 
-setup(name='XrayTo3DShape',
-      version="0.1.0",
-      description="Benchmarking utils and scripts for training Deep Learning Models for X-ray to 3D Shape Reconstruction",
-      url="https://github.com/naamiinepal/xrayto3D-benchmark",
-      author="Mahesh Shakya",
-      author_email="mahesh.shakya@naamii.org.np",
-      packages=find_packages(exclude=['build','docs']),
-      # install_requires=[
-      #       'simpleitk<2.1.0', # temporary fix ITK ERROR: ITK only supports orthonormal direction cosines. No orthonormal definition found! 
-      #       'omegaconf',
-      #       'nibabel',
-      #       'pandas'
-      # ],
-      keywords="Python benchmark biomedical",
-      classifiers=['Development Status :: 3 - Alpha',
-                   'Intended Audience :: Science/Research',
-                   'Topic :: Scientific/Engineering :: Medical Science Apps.',
-
-                   # Pick your license as you wish (should match "license" above)
-                   'License :: OSI Approved :: MIT License',
-
-                   # Specify the Python versions you support here. In particular, ensure
-                   # that you indicate whether you support Python 2, Python 3 or both.
-                   'Programming Language :: Python :: 3.6'
-                   ])
+setup(
+    name="XrayTo3DShape",
+    version="0.1.0",
+    description="Benchmarking utils and scripts for training Deep Learning Models for X-ray to 3D Shape Reconstruction",
+    url="https://github.com/naamiinepal/xrayto3D-benchmark",
+    author="Nepal Applied Mathematics and Informatics Institute for Research",
+    author_email="mahesh.shakya@naamii.org.np",
+    packages=find_packages(exclude=["build", "docs"]),
+    # install_requires=[
+    #       'simpleitk<2.1.0', # temporary fix ITK ERROR: ITK only supports orthonormal direction cosines. No orthonormal definition found!
+    #       'omegaconf',
+    #       'nibabel',
+    #       'pandas'
+    # ],
+    keywords="Python benchmark biomedical",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Medical Science Apps.",
+        # Pick your license as you wish (should match "license" above)
+        "License :: OSI Approved :: MIT License",
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        "Programming Language :: Python :: 3.6",
+    ],
+)
