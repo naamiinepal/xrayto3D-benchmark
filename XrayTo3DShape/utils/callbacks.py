cba36413ebf77d942ce438dd17954152cf7bcf21
mahesh shakya <mahesh.shakya@naamii.org.np>
2023-06-01 18:19:28 +0545

    closes #28

--- XrayTo3DShape/utils/callbacks.py
+++ XrayTo3DShape/utils/callbacks.py
@@ -1,10 +1,12 @@
 """custom pytorch-lightning callbacks for saving model prediction
 and evaluating metrics."""
+import copy
 import csv
 from pathlib import Path
 from typing import Any, Optional, Sequence
 
 import numpy as np
+import torch
 import pytorch_lightning as pl
 from monai.data.nifti_saver import NiftiSaver
 from monai.metrics.hausdorff_distance import HausdorffDistanceMetric
@@ -30,16 +32,20 @@ class NiftiPredictionWriter(BasePredictionWriter):
         write_interval: Literal["batch", "epoch", "batch_and_epoch"] = "batch",
         save_pred=True,
         save_gt=True,
+        image_size=64,
+        resolution=1.5,
     ) -> None:
         super().__init__(write_interval)
         self.output_dir = output_dir
         self.save_pred = save_pred
         self.save_gt = save_gt
+        self.image_size = image_size
+        self.resolution = resolution
 
         self.pred_nifti_saver = NiftiSaver(
             output_dir=self.output_dir,
             output_postfix="pred",
-            resample=True,
+            resample=False,
             dtype=np.int16,
             separate_folder=False,
         )
@@ -47,7 +53,7 @@ class NiftiPredictionWriter(BasePredictionWriter):
         self.gt_nifti_saver = NiftiSaver(
             output_dir=self.output_dir,
             output_postfix="gt",
-            resample=True,
+            resample=False,
             dtype=np.int16,
             separate_folder=False,
         )
@@ -62,9 +68,40 @@ class NiftiPredictionWriter(BasePredictionWriter):
         batch_idx: int,
         dataloader_idx: int,
     ) -> None:
-        metadict = {}
+        # rewrite metadict to reflect the actual shape and orientation of the volume
+        batch_size = len(prediction["seg_meta_dict"]["filename_or_obj"])
+        original_meta_dict = prediction["seg_meta_dict"]
+        metadict = copy.deepcopy(original_meta_dict)
         # metadict["filename_or_obj"] = prediction["seg_meta_dict"]["filename_or_obj"]
-        metadict = prediction["seg_meta_dict"]
+        metadict["spatial_shape"] = torch.from_numpy(
+            np.asarray(
+                [
+                    [self.image_size, self.image_size, self.image_size],
+                ]
+                * batch_size
+            )
+        )
+        metadict["space"] = [
+            "PIL",
+        ] * batch_size
+        metadict["affine"] = torch.from_numpy(
+            np.asarray(
+                [
+                    [
+                        [0, 0, self.resolution, 0],
+                        [-self.resolution, 0, 0, 0],
+                        [0, -self.resolution, 0, 0],
+                        [0, 0, 0, 1],
+                    ],
+                ]
+                * batch_size
+            )
+        )
+        # save the origin information too, which resides in the last column of the `affine matrix`
+
+        metadict["affine"][:, :, -1] = torch.tensor(
+            original_meta_dict["affine"][:, :, -1]
+        )
         if self.save_pred:
             self.pred_nifti_saver.save_batch(prediction["pred"], metadict)
         if self.save_gt:
@@ -121,6 +158,48 @@ class MetricsLogger(BasePredictionWriter):
     ) -> None:
         pred = prediction["pred"]
         gt = prediction["gt"]
+        subjects = self.get_filename(prediction)
+        dsc = to_numpy(self.DSC(pred, gt)).flatten().tolist()
+        asd = to_numpy(self.ASD(pred, gt)).flatten().tolist()
+        hd95 = to_numpy(self.HD95(pred, gt)).flatten().tolist()
+        nsd = [self.NSD(p, g) for p, g in zip(pred, gt)]
+        for row in zip(subjects, dsc, asd, hd95, nsd):
+            self.filestream_writer.writerow(
+                [
+                    f"{item:.2f}"
+                    if isinstance(item, (np.ndarray, np.float64, float))
+                    else item
+                    for item in row
+                ]
+            )
+
+
+class AnglePerturbationMetricsLogger(MetricsLogger):
+    """additionally record the angle perturbation in addition to metrics"""
+
+    def __init__(
+        self,
+        output_dir,
+        voxel_spacing,
+        nsd_tolerance=1.5,
+        write_interval: Literal["batch", "epoch", "batch_and_epoch"] = "batch",
+    ) -> None:
+        super().__init__(output_dir, voxel_spacing, nsd_tolerance, write_interval)
+
+    def write_on_batch_end(
+        self,
+        trainer: "pl.Trainer",
+        pl_module: "pl.LightningModule",
+        prediction: Any,
+        batch_indices: Optional[Sequence[int]],
+        batch: Any,
+        batch_idx: int,
+        dataloader_idx: int,
+    ) -> None:
+        pred = prediction["pred"]
+        gt = prediction["gt"]
+        print(prediction["seg_meta_dict"]["original_affine"])
+        print(prediction["seg_meta_dict"]["filename_or_obj"])
         subjects = self.get_filename(prediction)
         dsc = to_numpy(self.DSC(pred, gt)).flatten().tolist()
         asd = to_numpy(self.ASD(pred, gt)).flatten().tolist()
