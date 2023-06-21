90d4b2c1dfed5fb16206f2d53d5001edbde78b32
mahesh shakya <mahesh.shakya@naamii.org.np>
2023-06-21 14:17:31 +0545

    bash scripts and results

---
+++ bash_scripts/copy_results_and_merge.sh
@@ -0,0 +1,5 @@
+python scripts/copy_results.py --anatomy femur --tags dropout model-compare
+python scripts/copy_results.py --anatomy hip --tags dropout model-compare   
+python scripts/copy_results.py --anatomy vertebra --tags dropout model-compare
+python scripts/copy_results.py --anatomy rib --tags dropout model-compare     
+python scripts/merge_csv.py
